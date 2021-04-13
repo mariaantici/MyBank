148fbbba150eaeef13b375d4e92c21ade8283afc
Radu Vasile <radu.vasile9@yahoo.ro>
2021-04-13 21:21:16 +0300

    HomepageAdded

---
+++ Back/MyBankVer1/ViewModels/UserBalance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBank.ViewModels
+{
+    public class UserBalance
+    {
+        public string Name { get; set; }
+        public string CurrencyType { get; set; }
+        public double Amount { get; set; }
+
+        public UserBalance(string name, string curencyType, float amount)
+        {
+            this.Name = name;
+            this.CurrencyType = curencyType;
+            this.Amount = amount;
+        }
+    }
+}
